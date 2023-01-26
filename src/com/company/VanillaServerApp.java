cd8b54c33c7e5f0bd99ebd220a77a794f4785365
Clement <clement@leadiq.com>
2023-01-26 19:04:08 +0800

    fix(close-socket): close socket on server side if client side closes socket too

--- src/com/company/VanillaServerApp.java
+++ src/com/company/VanillaServerApp.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,20 +8,20 @@ public class VanillaServerApp {
 
     public static void streamInputAndPrint(Socket socket) throws IOException {
         // Stream the raw input data (bytes) from the client
-        try (InputStream is = socket.getInputStream()) {
+        InputStream is = socket.getInputStream();
+        try {
             // Add a buffer to the raw input data (bytes) stream
             BufferedInputStream bis = new BufferedInputStream(is);
             // Creats a DataInputStream Object
             // has convenience methods to deserialize the raw input data (bytes) into Java Primitives (e.g String)
             DataInputStream dis = new DataInputStream(bis);
-
             // Read the input stream of bytes into String
+            // Throws an EOFException when the socket is closed
             String line = dis.readUTF();
-
             System.out.println("line: " + line);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
+        } catch (EOFException e) {
+            // dis.readUTF() throws an EOFException when the client closes the on their end socket.
+            // clean up by closing the socket on our end
             socket.close();
         }
     }
@@ -43,4 +40,5 @@ public class VanillaServerApp {
 
         VanillaServerApp.streamInputAndPrint(socket);
     }
+
 }
